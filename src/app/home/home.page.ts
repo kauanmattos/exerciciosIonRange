2ff12ff1bb723200f4081fa2f1b92bccc1f73a6d
Kauan Mattos <kauanosmattos@gmail.com>
2025-03-24 22:02:06 -0300

    Ta bugadoo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RangeCustomEvent } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,10 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HomePage {
-
   constructor() {}
+  valorRange = 0;
 
+  onIonChange(ev: RangeCustomEvent) {
+    this.valorRange = parseInt(ev.detail.value.toString());
+  }
 }
