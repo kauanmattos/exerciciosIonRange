9f5c7b81970873f0662a9023652850ad7bcbd31c
Kauan Mattos <kauanosmattos@gmail.com>
2025-03-25 12:19:54 -0300

    Atualização do Código - Primeira Parte

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { RangeCustomEvent } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +7,27 @@ import { RangeCustomEvent } from '@ionic/angular';
   standalone: false,
 })
 export class HomePage {
-  constructor() {}
-  valorRange = 0;
+  valorOriginal: number = 0;
+  unidadeEntrada: string = 'km';
+  unidadeSaida: string = 'milhas'
+  valorConvertido: number = 0;
 
-  onIonChange(ev: RangeCustomEvent) {
-    this.valorRange = parseInt(ev.detail.value.toString());
+  constructor() { }
+
+  converterUnidades() {
+    this.valorOriginal = parseFloat(this.valorOriginal.toString());
+    this.calcularConversao();
+  }
+
+  calcularConversao() {
+    if (this.unidadeEntrada === 'km' && this.unidadeSaida === 'milhas') {
+      this.valorConvertido = this.valorOriginal * 0.621371;
+    } else if (this.unidadeEntrada === 'm' && this.unidadeSaida === 'pes') {
+      this.valorConvertido = this.valorOriginal * 3.28084;
+    } else if (this.unidadeEntrada === 'cm' && this.unidadeSaida === 'jardas') {
+      this.valorConvertido = this.valorOriginal * 0.0109361;
+    } else {
+      this.valorConvertido = this.valorOriginal; // Se as unidades forem as mesmas
+    }
   }
 }
